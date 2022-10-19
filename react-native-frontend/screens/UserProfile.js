ddb517590d2a14ec6b7c0a861fe9b738a95892db
cdemedeiros <cdemedeiros@uvic.ca>
2022-10-18 17:57:25 -0700

    added first features to account information page, text boxes and touchopacity button

--- react-native-frontend/screens/UserProfile.js
+++ react-native-frontend/screens/UserProfile.js
@@ -1,11 +1,66 @@
 import React from 'react';
-import { StyleSheet} from 'react-native';
-import { Provider as PaperProvider, Text } from 'react-native-paper';
+import { StyleSheet, Touchable, View, TouchableOpacity} from 'react-native';
+import { Provider as PaperProvider, Text, TextInput, Button } from 'react-native-paper';
+
 
 export default function UserProfile({navigation}){
+
+    const [name, nChange] = React.useState("Joe Guy");
+    const [quantity, qChange] = React.useState("4");
+
+    const styles = StyleSheet.create({
+        container: {
+            backgroundColor: '#393939',
+            display: 'flex',
+            height: '100%',
+            flexDirection: 'column'
+        },
+
+        textbox1: {
+            height: 40,
+            marginTop: 12,
+            marginLeft: 15,
+            marginRight: 15,
+            borderWidth: 1,
+            padding: 10
+        },
+        textbox2: {
+            height: 40,
+            marginTop: 60,
+            marginLeft: 15,
+            marginRight: 15,
+            borderWidth: 1,
+            padding: 10
+        },
+        saveButton: {
+            backgroundColor: '#7a42f4',
+            alignItems: "center",
+            fontSize: 25,
+            padding: 10,
+            margin: 80,
+            height: 60
+        }
+
+    })
     return (
         <PaperProvider>
-            <Text>Place holder for user profile view/edit feature</Text>
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.textbox1}
+                    onChangeText={nChange}
+                    value={name}
+                />
+                <TextInput
+                    style={styles.textbox2}
+                    onChangeText={qChange}
+                    value={quantity}
+                />
+                <TouchableOpacity
+                    style={styles.saveButton}
+                >
+                    <Text>Save</Text>
+                </TouchableOpacity>
+            </View>
         </PaperProvider>
     );
 }
