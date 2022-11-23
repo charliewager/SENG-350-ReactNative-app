3d8e34fcadf603c6afc6f704c71f2c96c69d0890
Josh Morgan <eston1007@gmail.com>
2022-11-23 13:47:23 -0800

    added backend endpoint for sending notifiction to user

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import smtplib
 app = Flask(__name__)
 
 
@@ -27,3 +28,39 @@ def find_carriers():
     ]
 
     return jsonify(users)
+
+@app.route('/sendhelpnotification', methods=['POST'])
+def send_help_notification():
+
+    users = [
+        {'name': 'Maria', 'naloxoneQuantity': 3, 'location': {'latitude': 48.4527997-0.0156, 'longitude': -123.3646428-0.0099}, 'isAvailable': True, 'phone': "7783501007@txt.bell.ca"},
+        {'name': 'Mohammed', 'naloxoneQuantity': 0, 'location': {'latitude': 48.4526994-0.0002, 'longitude': -123.3646428-0.0001}, 'isAvailable': True, 'phone': "7783501007@txt.bell.ca"},
+        {'name': 'Wei', 'naloxoneQuantity': 1, 'location': {'latitude': 48.4526994-0.0001, 'longitude': -123.3646428-0.0001}, 'isAvailable': False, 'phone': "7783501007@txt.bell.ca"},
+        {'name': 'Ana', 'naloxoneQuantity': 2, 'location': {'latitude': 48.4526994-0.0190, 'longitude': -123.3646428-0.0200}, 'isAvailable': True, 'phone': "7783501007@txt.bell.ca"},
+        {'name': 'John', 'naloxoneQuantity': 3, 'location': {'latitude': 48.4526994-0.0001, 'longitude': -123.3646428-0.0001}, 'isAvailable': True, 'phone': "7783501007@txt.bell.ca"},
+    ]
+    location = request.json.get('location')
+    recipient = request.json.get('recipient')
+
+    print(request.json)
+
+    for user in users:
+        if user['name'] == recipient:
+            send(f"Someone is overdosing at this location {location}", user['phone'])
+            return "sent"
+
+    return "recipient not found"
+
+
+def send(message, to):
+        # Replace the number with your own, or consider using an argument\dict for multiple people.
+    to_number = '7783501007@txt.bell.ca'
+    auth = ('seng350.helprequired@gmail.com', 'fpffiupiznugathp')
+
+    # Establish a secure session with gmail's outgoing SMTP server using your gmail account
+    server = smtplib.SMTP( "smtp.gmail.com", 587 )
+    server.starttls()
+    server.login(auth[0], auth[1])
+
+    # Send text message through SMS gateway of destination number
+    server.sendmail( auth[0], to_number, message)
